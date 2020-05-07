cc3699a66b82fcd760743976f2130a9f326cc1b4
raghav1299 <raghavsharma1299@gmail.com>
2020-05-07 15:16:47 +0530

    initial

--- src/screens/Signin.js
+++ src/screens/Signin.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {Component} from 'react';
 import { StyleSheet, View, Text, TouchableOpacity,Image } from 'react-native';
 import signup from './Signup'
@@ -17,32 +16,8 @@ export default class App extends Component {
         </TouchableOpacity>
         <TouchableOpacity onPress={()=>
             this.props.navigation.navigate('Signup')
-=======
-import React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native';
-import signup from './Signup'
-
-
-
-export default Sign = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.healX}>HealX</Text>
-      <Image style={{ width: 227, height: 222, marginTop: 16 }}
-        source={require('../assets/hlogo.png')}></Image>
-      <TouchableOpacity style={styles.touch1}
-        onPress={() =>
-          navigation.navigate('Login')
->>>>>>> 461eb899acca7854f457db50893a9432842a3d53
         }
-      >
-        <Text style={styles.signtext}>Sign in</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() =>
-        navigation.navigate('Signup')
-      }
         style={styles.touch2}>
-<<<<<<< HEAD
           <Text style={styles.signuptext}>Sign up</Text>
         </TouchableOpacity>
         <TouchableOpacity style={{justifyContent:'center'}}>
@@ -50,56 +25,47 @@ export default Sign = ({ navigation }) => {
         </TouchableOpacity>
       </View>
     )}
-=======
-        <Text style={styles.signuptext}>Sign up</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={{ justifyContent: 'center' }}>
-        <Text style={styles.areYouAPharmacistClickHere}>Are you a pharmacist? Click here.</Text>
-      </TouchableOpacity>
-    </View>
-  )
->>>>>>> 461eb899acca7854f457db50893a9432842a3d53
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    backgroundColor: "#ffffff"
-  },
-  healX: {
-    marginTop: 75,
-    width: 120,
-    height: 53,
-    fontFamily: "SegoeUI",
-    fontSize: 40,
-    fontWeight: "bold",
-    fontStyle: "normal",
-    lineHeight: 53,
-    letterSpacing: 0,
-    textAlign: "left",
-    color: "#385882"
-  },
-  touch1: {
-    marginTop: 65,
-    backgroundColor: "#b0bbe3",
-    width: 204,
-    height: 40,
-    borderRadius: 30,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  touch2: {
-    marginTop: 30,
-    width: 204,
-    height: 40,
-    borderRadius: 30,
-    backgroundColor: "#f6f7f9",
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  signtext: {
-    width: 75,
+    container: {
+      flex: 1,
+      alignItems: "center",
+      backgroundColor: "#ffffff"
+    },
+    healX: {
+      marginTop: 75,
+      width: 120,
+      height: 53,
+      fontFamily: "SegoeUI",
+      fontSize: 40,
+      fontWeight: "bold",
+      fontStyle: "normal",
+      lineHeight: 53,
+      letterSpacing: 0,
+      textAlign: "left",
+      color: "#385882"
+    },
+    touch1: {
+      marginTop: 65,
+      backgroundColor: "#b0bbe3",
+      width: 204,
+      height: 40,
+      borderRadius: 30,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    touch2: {
+      marginTop: 30,
+      width: 204,
+      height: 40,
+      borderRadius: 30,
+      backgroundColor: "#f6f7f9",
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    signtext:{
+      width: 75,
     height: 31,
     fontFamily: "Poppins",
     fontSize: 22,
@@ -108,9 +74,9 @@ const styles = StyleSheet.create({
     letterSpacing: 0,
     textAlign: "center",
     color: "#f5faff"
-  },
-  signuptext: {
-    width: 82,
+    },
+    signuptext:{
+      width: 82,
     height: 31,
     fontFamily: "Poppins",
     fontSize: 22,
@@ -120,19 +86,19 @@ const styles = StyleSheet.create({
     letterSpacing: 0,
     textAlign: "center",
     color: "#385882"
-  },
-  areYouAPharmacistClickHere: {
-    marginTop: 70,
-    //width: 297,
-    height: 24,
-    fontFamily: "OpenSans",
-    fontSize: 18,
-    fontWeight: "normal",
-    fontStyle: "normal",
-    lineHeight: 24,
-    letterSpacing: 0.63,
-    textAlign: "left",
-    color: "#385882"
-  }
-
-});
+    },
+    areYouAPharmacistClickHere: {
+      marginTop: 70,
+      //width: 297,
+      height: 24,
+      fontFamily: "OpenSans",
+      fontSize: 18,
+      fontWeight: "normal",
+      fontStyle: "normal",
+      lineHeight: 24,
+      letterSpacing: 0.63,
+      textAlign: "left",
+      color: "#385882"
+    }
+  
+  });
